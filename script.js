53b6a3661a238798a7cb1504b4c917e83beb7006
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-07-07 14:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+const newTodo=document.getElementById("newTodoInput");
+const addTodo=document.getElementById("addTodoBtn");
+const todoList=document.getElementById("todoList");
 
+
+addTodo.addEventListener('click', call);
+function call(){
+	if(newTodo.value!=="")
+	{
+	  const li=document.createElement("li");
+		li.innerText=newTodo.value;
+		todoList.appendChild(li);
+		newTodo.value="";
+	}
+}
